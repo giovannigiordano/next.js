df8fa4144c05ed398eb61831b5bfd70bcd52dde9
Giovanni Giordano <giordano.giovanni1993@gmail.com>
2018-10-09 11:46:51 +0200

    Update test

--- examples/with-jest/__tests__/index.test.js
+++ examples/with-jest/__tests__/index.test.js
@@ -1,8 +1,6 @@
 /* eslint-env jest */
-
 import { shallow } from 'enzyme'
 import React from 'react'
-import renderer from 'react-test-renderer'
 
 import App from '../pages/index.js'
 
@@ -11,13 +9,5 @@ describe('With Enzyme', () => {
     const app = shallow(<App />)
 
     expect(app.find('p').text()).toEqual('Hello World!')
-  })
-})
-
-describe('With Snapshot Testing', () => {
-  it('App shows "Hello world!"', () => {
-    const component = renderer.create(<App />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
   })
 })
